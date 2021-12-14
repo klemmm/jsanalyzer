380fdeaf227f318a28e28f1923a85b9c46f369b2
Clement Ballabriga <Clement.Ballabriga@7un.net>
2021-12-14 21:44:27 +0100

    ajout commentaires dans config.py

--- config.py
+++ config.py
@@ -1,13 +1,14 @@
-debug=False
+debug=False #Various debug info
 
+#List of enabled plugins
 enabled_plugins = [
     "default",
 ]
 
-max_iter = 1000
+max_iter = 1000 #Stop loop unrolling after that much iterations
 
-delete_unused = True
+delete_unused = True #Delete unused data from states (Performance will be really bad if you set this to False)
 
-console_enable = False
+console_enable = False #Show expressions passed to console.log
 
 
