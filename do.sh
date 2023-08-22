5cd6256f53f81cfc3ba6e3369827350ec0a90c0c
Raphaël Monat <raphael.monat@inria.fr>
2023-08-22 12:14:36 +0200

    Do.sh works from non-root directory too

--- do.sh
+++ do.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+ROOT_DIR="$(cd "$(dirname "$0")" && pwd -P)"
+
 NAME="$1"
 shift
-./analyze.py $NAME.js $NAME.pck
-./transform.py $* $NAME.pck $NAME.json
-node --stack-size=1000000 ./prettyprint.js $NAME.json $NAME-out.js
+$ROOT_DIR/analyze.py $NAME.js $NAME.pck
+$ROOT_DIR/transform.py $* $NAME.pck $NAME.json
+node --stack-size=1000000 $ROOT_DIR/prettyprint.js $NAME.json $NAME-out.js
 
